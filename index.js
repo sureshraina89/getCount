9a01a4848538d59e9dbce5e3da91e48247936297
sureshraina89 <sureshraina89@users.noreply.github.com>
2023-08-07 22:04:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello world!");
+  res.end("Hello world123!");
 });
 
 const PORT = process.env.PORT || 3000;
